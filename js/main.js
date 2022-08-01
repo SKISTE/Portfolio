7e29dce8361b0dedbc0450e1065f02d327f25869
SKISTE <skiste4u@gmail.com>
2022-08-01 05:45:39 +0400

    Text showed later

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let page = document.querySelector('html')
 document.addEventListener('scroll',()=>{
-    if(page.scrollTop>=400){
+    if(page.scrollTop>=470){
         document.querySelector('.secondmaintext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards'
         document.querySelector('.secondsubtext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards .3s'
     }
