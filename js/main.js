fdcd8a76e2a0afe297a839030587f328e4d43564
SKISTE <skiste4u@gmail.com>
2022-08-06 21:51:48 +0400

    Aa

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 let page = document.querySelector('html')
-document.addEventListener("DOMContentLoaded", function(event) { 
-    var widthWind = document.querySelector('body').offsetWidth;
-    console.log(widthWind)
-    if (widthWind <= 1000) {
-        document.addEventListener('scroll',()=>{
-            if(page.scrollTop>=1250){
-                document.querySelector('.secondmaintext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards'
-                document.querySelector('.secondsubtext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards .3s'
-            }
-          })
-          console.log('1')
+
+document.addEventListener('scroll', ()=>{
+    height1 = document.getElementById('first').offsetHeight
+    height2 = document.getElementById('second').offsetHeight/2
+    height = height1 + height2
+    if(page.scrollTop+window.innerHeight >= height){
+        document.querySelector('.secondmaintext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards'
+        document.querySelector('.secondsubtext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards .3s'
+
     }
-    else{
-        document.addEventListener('scroll',()=>{
-            if(page.scrollTop>=400){
-                document.querySelector('.secondmaintext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards'
-                document.querySelector('.secondsubtext').style.animation = 'showsecondtextarea 1s cubic-bezier(0.42, 0, 0.19, 2) forwards .3s'
-            }
-          })
-        console.log('2')
-    }
-});
+
+    //console.log(`height1 = ${height1}\nРазница = ${page.scrollTop-window.innerHeight}\nwindow.innerHeight = ${window.innerHeight}\ndocument.body.scrollTop${document.body.scrollTop}`)
+})
